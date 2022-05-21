7568acd2003f6168ed3eeed1e25510a4d8234f7a
valberjr <3cifhzht@duck.com>
2022-05-21 11:34:13 -0300

    atributos dinâmicos

--- 2-instalacao-cli/src/components/Info.vue
+++ 2-instalacao-cli/src/components/Info.vue
@@ -9,17 +9,28 @@
             <li>Vue</li>
         </ul>
         <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
+        <p>
+            Para acessarmeu portifólio
+            <a v-bind:href="meu_link">basta clicar aqui</a>
+        </p>
+        <Picture />
     </div>
 </template>
 
 <script>
+import Picture from './Picture.vue';
+
 export default {
     name: 'Info',
+    components: {
+        Picture,
+    },
     data() {
         return {
             esta_trabalhando: false,
             mostrar_email: true,
             email: 'junior@email.com',
+            meu_link: 'https://google.com',
         };
     },
 };
