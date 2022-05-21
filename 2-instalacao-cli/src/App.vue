66335a30dd58d1e2bcd67f61d4c3c26fa984e5b7
valberjr <3cifhzht@duck.com>
2022-05-21 10:33:48 -0300

    lifecycle hooks

--- 2-instalacao-cli/src/App.vue
+++ 2-instalacao-cli/src/App.vue
@@ -2,16 +2,19 @@
     <div>
         <PrimeiroComponente />
         <p>O dado de app é {{ testando }}</p>
+        <LifeCycle />
     </div>
 </template>
 
 <script>
 import PrimeiroComponente from './components/PrimeiroComponente.vue';
+import LifeCycle from './components/LifeCycle.vue';
 
 export default {
     name: 'App',
     components: {
         PrimeiroComponente,
+        LifeCycle,
     },
     data() {
         return {
