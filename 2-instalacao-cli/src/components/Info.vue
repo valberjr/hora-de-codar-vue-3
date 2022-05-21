6b4d6f8aeed2a8e54986bd824ba13a47fb67e263
valberjr <3cifhzht@duck.com>
2022-05-21 11:07:47 -0300

    hierarquia de components

---
+++ 2-instalacao-cli/src/components/Info.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>Estou trabalhando no momento.</p>
+        <p>Utilizo as seguintes Tecnologias:</p>
+        <ul>
+            <li>Angular</li>
+            <li>React</li>
+            <li>Vue</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Info',
+};
+</script>
---
+++ 2-instalacao-cli/src/components/Pessoa.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>Esta á descrição da pessoa: {{ nome }}</h2>
+        <Info />
+        <Form />
+    </div>
+</template>
+
+<script>
+import Info from './Info.vue';
+import Form from './Form.vue';
+
+export default {
+    name: 'Pessoa',
+    components: {
+        Info,
+        Form,
+    },
+    data() {
+        return {
+            nome: 'Junior',
+        };
+    },
+};
+</script>
