7932a1f44faf53a099e96078683108a4ac17fb30
valberjr <3cifhzht@duck.com>
2022-05-21 11:20:47 -0300

    diretivas (v-if, v-show)

--- 2-instalacao-cli/src/components/Info.vue
+++ 2-instalacao-cli/src/components/Info.vue
@@ -1,17 +1,26 @@
 <template>
     <div>
-        <p>Estou trabalhando no momento.</p>
+        <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+        <p v-else>Estou em busca de novas oportunidades</p>
         <p>Utilizo as seguintes Tecnologias:</p>
         <ul>
             <li>Angular</li>
             <li>React</li>
             <li>Vue</li>
         </ul>
+        <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
     </div>
 </template>
 
 <script>
 export default {
     name: 'Info',
+    data() {
+        return {
+            esta_trabalhando: false,
+            mostrar_email: true,
+            email: 'junior@email.com',
+        };
+    },
 };
 </script>
