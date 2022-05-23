9dbcb380f8cb4023af500b0d09ec2497b00242ba
valberjr <3cifhzht@duck.com>
2022-05-23 09:41:49 -0300

    emit

--- 2-instalacao-cli/src/components/Picture.vue
+++ 2-instalacao-cli/src/components/Picture.vue
@@ -1,23 +1,28 @@
 <template>
     <div>
         <img :src="avatar" :alt="descricao" />
-        <Reutilizacao />
+        <MudarImagem @mudar-imagem="trocarImagem" />
     </div>
 </template>
 
 <script>
-import Reutilizacao from './Reutilizacao.vue';
+import MudarImagem from './MudarImagem.vue';
 
 export default {
     name: 'Picture',
     components: {
-        Reutilizacao,
+        MudarImagem,
     },
     data() {
         return {
             avatar: '/img/avatar.png',
             descricao: 'Avatar',
         };
+    },
+    methods: {
+        trocarImagem() {
+            this.avatar = '/img/avatar2.png';
+        },
     },
 };
 </script>
