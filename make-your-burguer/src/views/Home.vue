603ede618cc02200b92185593c97846c19f802d4
valberjr <3cifhzht@duck.com>
2022-05-23 17:35:45 -0300

    criando o banner da aplicação

--- make-your-burguer/src/views/Home.vue
+++ make-your-burguer/src/views/Home.vue
@@ -1,12 +1,18 @@
 <template>
+    <Banner />
     <div class="main-container">
-        <h1>HOME</h1>
+        <h1>Monte o seu burguer:</h1>
+        <p>formulario</p>
     </div>
 </template>
 
 <script>
+import Banner from '../components/Banner.vue';
+
 export default {
     name: 'Home',
-    components: {},
+    components: {
+        Banner,
+    },
 };
 </script>
