bc7a54227e981ccc5cb24abfbcd9e97423c2fa0d
valberjr <3cifhzht@duck.com>
2022-05-23 16:54:51 -0300

    implementando o vue router

---
+++ make-your-burguer/src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <router-link to="/">
+            <img src="" id="logo" />
+        </router-link>
+        <nav id="nav">
+            <router-link to="/">Home</router-link> |
+            <router-link to="/pedidos">Pedidos</router-link>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+};
+</script>
+
+<style scoped></style>
