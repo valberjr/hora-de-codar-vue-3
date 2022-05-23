8cff3eb4f9a8ace4f0dea4310e694af1c5c192a1
valberjr <3cifhzht@duck.com>
2022-05-23 08:35:45 -0300

    eventos (@submit)

--- 2-instalacao-cli/src/components/Form.vue
+++ 2-instalacao-cli/src/components/Form.vue
@@ -1,17 +1,20 @@
 <template>
     <div>
         <h2>Meu formulário:</h2>
-        <form action="">
+        <form action="" @submit="enviarFormulario($event)">
             <div>
-                <InputText />
+                <input type="text" v-model="name" />
             </div>
             <div>
-                <InputText />
+                <input type="text" v-model="email" />
             </div>
             <div>
                 <Submit />
             </div>
         </form>
+        <p>Conteúdo</p>
+        <p>Conteúdo</p>
+        <p>Conteúdo</p>
     </div>
 </template>
 
@@ -25,5 +28,29 @@ export default {
         InputText,
         Submit,
     },
+    data() {
+        return {
+            name: '',
+            email: '',
+        };
+    },
+    methods: {
+        enviarFormulario(e) {
+            e.preventDefault();
+
+            const name = this.name;
+            const email = this.email;
+
+            console.log('Formulário enviados');
+            console.log('O nome é: ' + name);
+            console.log('O email é: ' + email);
+        },
+    },
 };
 </script>
+
+<style scoped>
+p {
+    margin: 150px;
+}
+</style>
