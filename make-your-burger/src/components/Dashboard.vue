0df0d99e8479de3764b91f7982491320d71f5413
valberjr <3cifhzht@duck.com>
2022-05-24 10:50:55 -0300

    atualização de pedidos

--- make-your-burger/src/components/Dashboard.vue
+++ make-your-burger/src/components/Dashboard.vue
@@ -30,12 +30,16 @@
                         </ul>
                     </div>
                     <div>
-                        <select name="status" class="status">
+                        <select
+                            name="status"
+                            class="status"
+                            @change="updateBurger($event, burger.id)"
+                        >
                             <option value="">Selecione</option>
                             <option
                                 v-for="s in status"
                                 :key="s.id"
-                                value="s.tipo"
+                                :value="s.tipo"
                                 :selected="burger.status === s.tipo"
                             >
                                 {{ s.tipo }}
@@ -94,6 +98,20 @@ export default {
             // msg
 
             this.getPedidos();
+        },
+
+        async updateBurger(event, id) {
+            const option = event.target.value;
+
+            const dataJson = JSON.stringify({ status: option });
+
+            const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+                method: 'PATCH',
+                headers: { 'Content-Type': 'application/json' },
+                body: dataJson,
+            });
+
+            const res = await req.json();
         },
     },
     mounted() {
