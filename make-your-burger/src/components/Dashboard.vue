d5bf5f07efa0da75b13238c7f4f02b341b477254
valberjr <3cifhzht@duck.com>
2022-05-24 10:34:31 -0300

    recebendo os status dos pedidos

--- make-your-burger/src/components/Dashboard.vue
+++ make-your-burger/src/components/Dashboard.vue
@@ -32,6 +32,14 @@
                     <div>
                         <select name="status" class="status">
                             <option value="">Selecione</option>
+                            <option
+                                v-for="s in status"
+                                :key="s.id"
+                                value="s.tipo"
+                                :selected="burger.status === s.tipo"
+                            >
+                                {{ s.tipo }}
+                            </option>
                         </select>
                         <button class="delete-btn">Cancelar</button>
                     </div>
@@ -60,6 +68,15 @@ export default {
             this.burgers = data;
 
             // resgatar os status
+            this.getStatus();
+        },
+
+        async getStatus() {
+            const req = await fetch('http://localhost:3000/status');
+
+            const data = await req.json();
+
+            this.status = data;
         },
     },
     mounted() {
